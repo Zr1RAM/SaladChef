5a03cb34c00d4deafe1bd15537e632674f57c4b6
Zr1RAM <sriram.varadarajulu@gmail.com>
2018-11-14 11:28:47 +0800

    Initial Commit
    First commit to the project done on day 2. Created Game Manager so far with Game Camera controls function still being implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager gameManager;
+	// Use this for initialization
+	void Awake ()
+    {
+		if(gameManager == null)
+        {
+            gameManager = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameCamera();
+	}
+    [SerializeField]
+    Transform Player1, Player2;
+    void GameCamera()
+    {
+        if(Player2 != null && Player1 != null)
+        {
+            Camera.main.transform.position = new Vector3((Player1.position.x + Player2.position.x) / 2, Camera.main.transform.position.y, (Player1.position.z + Player2.position.z) / 2);
+        }
+    }
+}
